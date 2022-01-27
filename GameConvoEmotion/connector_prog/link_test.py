540e5242c15a03b5eb744d681e739aed4144f28c
qcxc123 <qcxc123@gmail.com>
2022-01-27 17:14:20 +0800

    added GameConvoEmotion merged

---
+++ GameConvoEmotion/connector_prog/link_test.py
@@ -0,0 +1,214 @@
+## Run this command in terminal before executing this program
+## rasa run --enable-api
+## and also run this in seperate terminal
+## rasa run actions
+
+import requests
+from emotion_recognition import init_emotion, get_emotion_class
+#import subprocess
+from threading import Thread, Event
+from play_audio import playsound
+import pyaudio
+from deepspeech import Model
+import scipy.io.wavfile as wav
+import wave
+import os
+import torch
+#import time
+from TTS.tts.utils.generic_utils import setup_model
+from TTS.utils.io import load_config
+from TTS.tts.utils.text.symbols import symbols, phonemes
+from TTS.utils.audio import AudioProcessor
+from TTS.tts.utils.synthesis import synthesis
+from TTS.vocoder.utils.generic_utils import setup_generator
+
+'''Define Functions'''
+def tts(model, text, CONFIG, use_cuda, ap, OUT_FILE, use_gl):
+    #t_1 = time.time()
+    waveform, alignment, mel_spec, mel_postnet_spec, stop_tokens, inputs = synthesis(model, text, CONFIG, use_cuda, ap, speaker_id, style_wav=None, truncated=False, enable_eos_bos_chars=CONFIG.enable_eos_bos_chars)
+    # mel_postnet_spec = ap.denormalize(mel_postnet_spec.T)
+    if not use_gl:
+        waveform = vocoder_model.inference(torch.FloatTensor(mel_postnet_spec.T).unsqueeze(0))
+        waveform = waveform.flatten()
+    if use_cuda:
+        waveform = waveform.cpu()
+    waveform = waveform.numpy()
+    #rtf = (time.time() - t_1) / (len(waveform) / ap.sample_rate)
+    #tps = (time.time() - t_1) / len(waveform)
+    #print(waveform.shape)
+    #print(" > Run-time: {}".format(time.time() - t_1))
+    #print(" > Real-time factor: {}".format(rtf))
+    #print(" > Time per step: {}".format(tps))
+    #IPython.display.display(IPython.display.Audio(waveform, rate=CONFIG.audio['sample_rate']))
+    ap.save_wav(waveform, OUT_FILE)  
+    return alignment, mel_postnet_spec, stop_tokens, waveform
+
+def record_audio(WAVE_OUTPUT_FILENAME):
+	CHUNK = 1024
+	FORMAT = pyaudio.paInt16
+	CHANNELS = 1
+	RATE = 16000
+	RECORD_SECONDS = 8
+
+	p = pyaudio.PyAudio()
+
+	stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+	print("Recording")
+	frames = [stream.read(CHUNK) for i in range(0, int(RATE / CHUNK * RECORD_SECONDS))]
+	print("Done recording")
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+
+	wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+	wf.setnchannels(CHANNELS)
+	wf.setsampwidth(p.get_sample_size(FORMAT))
+	wf.setframerate(RATE)
+	wf.writeframes(b''.join(frames))
+	wf.close()
+
+def get_input():
+    global message, finished
+    if not finished.isSet():
+        #message = input("Your input: \n")
+        '''STT block'''
+    	# read user input
+        record_audio(WAVE_OUTPUT_FILENAME)
+    	
+    	# convert audio to text
+        N_FEATURES = 25
+        N_CONTEXT = 9
+        BEAM_WIDTH = 500
+        LM_ALPHA = 0.75
+        LM_BETA = 1.85
+        ds = Model(model_file_path)
+        fs, audio = wav.read(WAVE_OUTPUT_FILENAME)
+        message = ds.stt(audio)
+    	
+        # remove audio file
+        os.remove(WAVE_OUTPUT_FILENAME)
+        
+    	# for reference
+        print("I say: "+ message)
+        finished.set()
+    return
+    
+def pred_emotion(vs, detector, predictor, models):
+    global message, finished
+    c = 0
+    pred = "neutral"
+    while not finished.isSet():        
+        frame = vs.read()
+        emotion_class = get_emotion_class(frame, detector, predictor, models)
+        if emotion_class != "neutral":
+            c += 1 if emotion_class == pred else 0
+            pred = emotion_class
+            if c==5:
+                message = "I'm " + emotion_class            
+                print(message)
+                finished.set()
+    return emotion_class
+
+'''Settings for STT'''
+WAVE_OUTPUT_FILENAME = "user_audio.wav"
+model_file_path = 'stt_tts_data/deepspeech-0.9.3-models.pbmm'
+
+'''Load TTS'''
+# runtime settings
+use_cuda = False
+
+# model paths
+TTS_MODEL = "stt_tts_data/tts_model.pth.tar"
+TTS_CONFIG = "stt_tts_data/config.json"
+VOCODER_MODEL = "stt_tts_data/vocoder_model.pth.tar"
+VOCODER_CONFIG = "stt_tts_data/config_vocoder.json"
+OUT_FILE = "bot_reply.wav"
+
+# load configs
+TTS_CONFIG = load_config(TTS_CONFIG)
+VOCODER_CONFIG = load_config(VOCODER_CONFIG)
+
+# load the audio processor
+TTS_CONFIG.audio['stats_path'] = 'stt_tts_data/scale_stats.npy'
+ap = AudioProcessor(**TTS_CONFIG.audio)   
+
+# LOAD TTS MODEL
+# multi speaker 
+speaker_id = None
+speakers = []
+
+# load the model
+num_chars = len(phonemes) if TTS_CONFIG.use_phonemes else len(symbols)
+model = setup_model(num_chars, len(speakers), TTS_CONFIG)
+
+# load model state
+cp =  torch.load(TTS_MODEL, map_location=torch.device('cpu'))
+
+# load the model
+model.load_state_dict(cp['model'])
+if use_cuda:
+    model.cuda()
+model.eval()
+
+# set model stepsize
+if 'r' in cp:
+    model.decoder.set_r(cp['r'])
+
+# LOAD VOCODER MODEL
+vocoder_model = setup_generator(VOCODER_CONFIG)
+vocoder_model.load_state_dict(torch.load(VOCODER_MODEL, map_location="cpu")["model"])
+vocoder_model.remove_weight_norm()
+vocoder_model.inference_padding = 0
+
+ap_vocoder = AudioProcessor(**VOCODER_CONFIG['audio'])    
+if use_cuda:
+    vocoder_model.cuda()
+vocoder_model.eval()
+
+
+'''Connector Program'''
+
+bot_message = ""
+message=""
+
+bye_list = ["Bye", "Goodbye", "See you again!", "Let's talk again next time!"]
+
+finished = Event()
+vs, detector, predictor, models = init_emotion()
+
+while bot_message not in bye_list:
+
+    while not finished.isSet():   
+        worker = Thread(target=get_input)
+        worker.setDaemon(True)
+        worker.start()
+        emotion_class = pred_emotion(vs, detector, predictor, models)
+
+    if len(message)==0:
+        continue
+    print("Sending message now...")
+
+    #Pass message to rasa and print response
+    r = requests.post('http://localhost:5005/webhooks/rest/webhook', json={"message": message})
+    for i in r.json():
+        bot_message = i['text']
+        print(f"{bot_message}")
+    
+    #Get rasa slot for emotion detection mode
+    url = "http://localhost:5005/conversations/default/tracker?include_events=NONE"
+    s = requests.request("GET", url, headers={}, data={})
+    j = s.json()
+    emo_mode = j['slots']['emo_mode']
+    print(f"Emotion Detection mode: {emo_mode}")
+
+    '''TTS block'''
+    sentence = bot_message
+    align, spec, stop_tokens, wavform = tts(model, sentence, TTS_CONFIG, use_cuda, ap, OUT_FILE, use_gl=False)
+    # Playing the converted file
+    playsound("bot_reply.wav")
+
+    finished = Event() # reset event
