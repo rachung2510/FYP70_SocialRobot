97fa420f7f203c760505af6f680e986c932c7cb7
rachung2510 <chungrc.rachel@gmail.com>
2022-04-08 15:30:40 +0800

    change formats and colours

--- connector_prog/Nothing_Detection.py
+++ connector_prog/Nothing_Detection.py
@@ -2,10 +2,14 @@ import cv2
 import numpy as np
 import random
 import time
+from VideoCapture import VideoCapture
 
 def SimonSays_nothing(selected_item,cam):
 
     WINDOW = "Simon Says"
+
+#    items_of_selection = ["bottle","sports ball","cell phone","spoon","fork"]
+#    selected_item = random.choice(items_of_selection)
 
     lst = ["rectangular object","cylindrical object","spherical object"]
     choose = selected_item
@@ -17,34 +21,22 @@ def SimonSays_nothing(selected_item,cam):
         yolo = cv2.dnn.readNet('./object_data/yolov4.weights' , './object_data/yolov4.cfg')
         with open("./object_data/coco.names","r") as f:
             classes = f.read().splitlines()
-        # print((classes))
-
     else:
         yolo = cv2.dnn.readNet('./object_data/iteration3_final.weights' , './object_data/customyolov4-obj.cfg')
         with open("./object_data/obj.names","r") as f:
             classes = f.read().splitlines()
-        # print((classes))
 
     starting_time = time.time()
 
     width, height = 640, 480
 
-#    items_of_selection = ["bottle","sports ball","spoon","fork"]
-#    items_of_selection = ["bottle","sports ball","cell phone"]
-#    selected_item = random.choice(items_of_selection)
-
 #    cv2.namedWindow(WINDOW, cv2.WND_PROP_FULLSCREEN)
 #    cv2.setWindowProperty(WINDOW, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
+    frame_cnt = 0
     while(True):
         frame = cam.read()
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-        if time.time() - starting_time > 30:
-            print("Time's Up!")
-            print('Time taken: %.2f' % (time.time() - starting_time))
-            break
+        frame_cnt += 1
 
         blob = cv2.dnn.blobFromImage(frame,1/255, (320,320), (0,0,0), swapRB=True, crop=False)
         yolo.setInput(blob)
@@ -53,7 +45,6 @@ def SimonSays_nothing(selected_item,cam):
         boxes = []
         confidences = []
         class_ids = []
-
         for output in layeroutput:
             for detection in output:
                 score = detection[5:]
@@ -72,44 +63,63 @@ def SimonSays_nothing(selected_item,cam):
                     confidences.append(float(confidence))
                     class_ids.append(class_id)
 
-        #print(len(boxes))
         indexes = cv2.dnn.NMSBoxes(boxes,confidences,0.5,0.4)
 
         color = (0, 0, 255)
 
-#        cv2.putText(frame, "Please find this Item: " + choose, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
+        cv2.putText(frame, "Please find this item: " + choose, (0,25), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,0), 2)
+
         font = cv2.FONT_HERSHEY_PLAIN
         colors = np.random.uniform(0,255,size=(len(boxes),3))
-        if len(indexes) >0:
+        print('[%d]Detected:' % frame_cnt, end="")
+        first_item = 1
+        if len(indexes) > 0:
             for i in indexes.flatten():
                 x,y,w,h = boxes[i]
 
                 label = str(classes[class_ids[i]])
                 confi = str(round(confidences[i],2))
-                #color = colors[i]
+                print(('' if first_item else ','), label, end="")
+                first_item = 0
+
                 if label not in sample:
                     sample.append(label)
 
-#                cv2.rectangle(frame,(x,y),(x+w,y+h),color,2)
-#                cv2.putText(frame, label + " " + confi, (x,y+20), font, 2, (255,255,255), 2)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),color,2)
+                cv2.putText(frame, label + " " + confi, (x,y+20), font, 2, (255,255,255), 2)
+        else:
+            print(" none", end="")
 
-        if time.time() - starting_time > 10:
-            if choose not in sample:
-#                cv2.putText(frame, "Well Done!!!", (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
-                ans = True
-            else:
-#                cv2.putText(frame, "Wrong!!!", (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
-                ans = False
+        if choose in sample:
+#            print("\nI didn't say Simon Says! Listen carefully next time :)")
+            cv2.putText(frame, "Wrong!", (180,240), cv2.FONT_HERSHEY_COMPLEX, 2, (0,0,255), 6)
+            ans = False
             break
-        # print(sample)
+#            cv2.imshow(WINDOW, frame)
+#            cv2.waitKey(1)
+#            choose = random.choice(items_of_selection)
+#            sample = []
+#            continue
+        elif time.time() - starting_time > 20:
+#            print("\nTime's up! You won!")
+            cv2.putText(frame, "Well Done!", (130,240), cv2.FONT_HERSHEY_COMPLEX, 2, (0,255,0), 6)
+            ans = True
+            break
 
+        # Display the frame.
 #        cv2.imshow(WINDOW, frame)
 #        if cv2.waitKey(1) & 0xFF == ord('q'):
 #            break
 
+        print(" "*100, end="\r") # padding
+
+    print()
+#    cv2.imshow(WINDOW, frame)
+#    cv2.waitKey(1)
+#    time.sleep(2)
+#    cv2.destroyAllWindows()
     return ans
 
-#cam = USBCamera(capture_device=0, width=640, height=480)
-#SimonSays_nothing("cell_phone", cam)
-#cam.cap.release()
-#cv2.destroyAllWindows()
+#cam = VideoCapture(0)
+#SimonSays_nothing("cell phone", cam)
+#cam.release()
--- connector_prog/Object_Detection.py
+++ connector_prog/Object_Detection.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import time
-from jetcam.usb_camera import USBCamera
 import random
 from VideoCapture import VideoCapture
 
@@ -12,8 +11,8 @@ def SimonSays_item(selected_item, cam):
     starting_time = time.time()
     width, height = 640, 480
 
-#items_of_selection = ["bottle","sports ball","spoon","fork"]
-#    items_of_selection = ["bottle","sports ball","cell phone","backpack", 'cylindrical object', 'rectangular object', 'spherical object']
+#    items_of_selection = ["bottle","sports ball","cell phone","spoon","fork"]
+#    items_of_selection = ["bottle","sports ball","cell phone","backpack","cylindrical object","rectangular object","spherical object"]
 #    selected_item = random.choice(items_of_selection)
 
     lst = ["rectangular object","cylindrical object","spherical object"]
@@ -24,23 +23,23 @@ def SimonSays_item(selected_item, cam):
         yolo = cv2.dnn.readNet('./object_data/yolov4.weights' , './object_data/yolov4.cfg')
         with open("./object_data/coco.names","r") as f:
             classes = f.read().splitlines()
-        # print((classes))
     else:
         yolo = cv2.dnn.readNet('./object_data/iteration3_final.weights' , './object_data/customyolov4-obj.cfg')
         with open("./object_data/obj.names","r") as f:
             classes = f.read().splitlines()
-        # print((classes))
 
 #    cv2.namedWindow(WINDOW, cv2.WND_PROP_FULLSCREEN)
 #    cv2.setWindowProperty(WINDOW, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
+    frame_cnt = 0
     while(True):
 
         frame = cam.read()
+        frame_cnt += 1
 
         if (time.time() - starting_time) > 120:
             print("\nTime's Up!")
-            print('Time taken: %.2f' % (time.time() - starting_time))
+            cv2.putText(frame, "Time out :(", (130,240), cv2.FONT_HERSHEY_COMPLEX, 2, (0,0,255), 6)
             break
 
         blob = cv2.dnn.blobFromImage(frame, 1/255, (320,320), (0,0,0), swapRB=True, crop=False)
@@ -70,46 +69,49 @@ def SimonSays_item(selected_item, cam):
                     confidences.append(float(confidence))
                     class_ids.append(class_id)
 
-        #print(len(boxes))
         indexes = cv2.dnn.NMSBoxes(boxes,confidences,0.5,0.4)
 
         color = (0, 0, 255)
 
-#        cv2.putText(frame, "Please find this Item: " + selected_item, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255))
+        cv2.putText(frame, "Please find this item: " + selected_item, (0,25), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,0), 2)
+
         font = cv2.FONT_HERSHEY_PLAIN
         colors = np.random.uniform(0,255,size=(len(boxes),3))
-        print('Detected: ', end="")
+        print('[%d]Detected:' % frame_cnt, end="")
+        first_item = 1
         if len(indexes) > 0:
             for i in indexes.flatten():
                 x,y,w,h = boxes[i]
 
                 label = str(classes[class_ids[i]])
                 confi = str(round(confidences[i],2))
-                #color = colors[i]
-                print(label, end=", ")
+                print(('' if first_item else ','), label, end="")
+                first_item = 0
+
                 if label == selected_item:
-                    print('\nMatch!')
-                    print('Time taken: %.2f' % (time.time() - starting_time))
-                    color = (0, 255, 0)
-#                    cv2.rectangle(frame,(x,y),(x+w,y+h),color,2)
-#                    time.sleep(2)
-#                    start = time.time()
-#                    while (time.time() - start > 5000000):
-#                        cv2.rectangle(frame,(x,y),(x+w,y+h),color,2)
+                    print('\nMatch! Time taken: %.2fs' % (time.time() - starting_time))
+                    cv2.putText(frame, "Item found!", (130,240), cv2.FONT_HERSHEY_COMPLEX, 2, (0,255,0), 6)
+                    cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+                    cv2.putText(frame, label + " " + confi, (x,y+20), font, 2, (255,255,255), 2)
 #                    selected_item = random.choice(items_of_selection)
-                    status_level=0
-#                    cv2.destroyAllWindows()
+#                    continue
                     return True
 
-#                cv2.rectangle(frame,(x,y),(x+w,y+h),color,2)
-#                cv2.putText(frame, label + " " + confi, (x,y+20), font, 2, (255,255,255), 2)
+                cv2.rectangle(frame,(x,y),(x+w,y+h),color,2)
+                cv2.putText(frame, label + " " + confi, (x,y+20), font, 2, (255,255,255), 2)
+        else:
+            print(" none", end="")
 
 #        cv2.imshow(WINDOW, frame)
 #        if cv2.waitKey(1) & 0xFF == ord('q'):
+#            print()
 #            break
 
-        print(" "*100, end="\r")
+        print(" "*100, end="\r") # padding
 
+#    cv2.imshow(WINDOW, frame)
+#    cv2.waitKey(1)
+#    time.sleep(2)
 #    cv2.destroyAllWindows()
     return False
 
@@ -119,6 +121,5 @@ def get_time(prev):
     return curr
 
 #cam = VideoCapture(0)
-#SimonSays_item(None, cam)
+#SimonSays_item("spoon", cam)
 #cam.release()
-#cv2.destroyAllWindows()
